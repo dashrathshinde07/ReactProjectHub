607265de76b1c5d6f37a7358622872f24663299d
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-05-14 16:26:18 +0530

    program section is done

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import Programs from "./Components/Programs/Programs";
+import Title from "./Components/Title/Title";
 
 const App = () => {
   return (
     <>
       <div>
-        <Navbar/>
-        <Hero/>
+        <Navbar />
+        <Hero />
+        <div className="container">
+          <Title subTitle="OUR PROGRAM" title="What We Offer" />
+          <Programs />
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
