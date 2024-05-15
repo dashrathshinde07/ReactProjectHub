b57a8164f1ea4dea2ee99e168d234a2b7edf3e74
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-05-15 09:46:15 +0530

    In contact section done form section also and mainly for the form we use web3 forms external api for getting an real message on our own mail id

--- edusity/src/Components/Contact/Contact.jsx
+++ edusity/src/Components/Contact/Contact.jsx
@@ -1,10 +1,37 @@
+import React from "react";
 import "./Contact.css";
 import msg_icon from "../../assets/msg-icon.png";
 import mail_icon from "../../assets/mail-icon.png";
 import phone_icon from "../../assets/phone-icon.png";
 import location_icon from "../../assets/location-icon.png";
+import white_arrow from "../../assets/white-arrow.png";
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "62480016-caa2-4037-839d-6aa386bf1e64");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
     <>
       <div className="contact">
@@ -19,10 +46,55 @@ const Contact = () => {
             university community.
           </p>
           <ul>
-            <li><img src={mail_icon} alt="" />Contact@GreatStack.dev </li>
-            <li><img src={phone_icon} alt="" />+1 123-456-7890</li>
-            <li><img src={location_icon} alt="" />77 Massachusetts Ave, Cambridge <br /> MA 02139, United States</li>
+            <li>
+              <img src={mail_icon} alt="" />
+              Contact@GreatStack.dev{" "}
+            </li>
+            <li>
+              <img src={phone_icon} alt="" />
+              +1 123-456-7890
+            </li>
+            <li>
+              <img src={location_icon} alt="" />
+              77 Massachusetts Ave, Cambridge <br /> MA 02139, United States
+            </li>
           </ul>
+        </div>
+        <div className="contact-col">
+          <form onSubmit={onSubmit}>
+            <label>Your name</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              required
+            />
+            <label>Phone Number</label>
+            <input
+              type="tel"
+              placeholder="Enter your mobile number"
+              name="phone"
+              required
+            />
+            <label>Your Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email id"
+              required
+            />
+            <label>Write your message</label>
+            <textarea
+              name="message"
+              rows="6"
+              placeholder="Enter your message"
+              required
+            ></textarea>
+            <button type="submit" className="btn dark-btn">
+              Sumbit Now <img src={white_arrow} alt="" />
+            </button>
+          </form>
+          <span>{result}</span>
         </div>
       </div>
     </>
