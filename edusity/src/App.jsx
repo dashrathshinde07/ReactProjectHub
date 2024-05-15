d530b4f5d67b89050083f7776be4af4ccc62f090
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-05-15 08:27:51 +0530

    Testimonials section is done

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+// import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
 import Title from "./Components/Title/Title";
 import About from "./Components/About/About";
 import Campus from "./Components/Campus/Campus";
+import Testimonials from "./Components/Testimonials/Testimonials";
 
 const App = () => {
   return (
@@ -17,7 +18,9 @@ const App = () => {
           <Programs />
           <About />
           <Title subTitle="GALLERY" title="Campus Photos" />
-          <Campus/>
+          <Campus />
+          <Title subTitle="TESTIMONIALS" title="What Student Says" />
+          <Testimonials/>
         </div>
       </div>
     </>
