64ae32a9123658736f36a4d9530a64da2fc9ecc7
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-05-15 08:59:49 +0530

    In contact section the contact detail section is done

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -6,6 +6,7 @@ import Title from "./Components/Title/Title";
 import About from "./Components/About/About";
 import Campus from "./Components/Campus/Campus";
 import Testimonials from "./Components/Testimonials/Testimonials";
+import Contact from "./Components/Contact/Contact";
 
 const App = () => {
   return (
@@ -20,7 +21,9 @@ const App = () => {
           <Title subTitle="GALLERY" title="Campus Photos" />
           <Campus />
           <Title subTitle="TESTIMONIALS" title="What Student Says" />
-          <Testimonials/>
+          <Testimonials />
+          <Title subTitle="CONTACT US" title="Get in Touch" />
+          <Contact/>
         </div>
       </div>
     </>
