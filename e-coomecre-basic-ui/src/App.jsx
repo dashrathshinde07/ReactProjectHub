f0418d00cc9d4ce36f5ec50c99bc81508f885468
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-05-20 18:09:57 +0530

    basic UI styling is done

--- e-coomecre-basic-ui/src/App.jsx
+++ e-coomecre-basic-ui/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Nav from "./components/Nav";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Details from "./components/Details";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <div className="h-screen w-screen flex">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/details/:id" element={<Details/>}/>
+        </Routes>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
