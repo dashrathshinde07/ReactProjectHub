471ff1e7cc485e8b3877f125e86b0c3ae2cb6679
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-05-25 23:03:44 +0530

    #

--- contact-us-landing-react/src/components/Button/Button.jsx
+++ contact-us-landing-react/src/components/Button/Button.jsx
@@ -1,12 +1,14 @@
 import styles from "./Button.module.css";
 import { MdMessage } from "react-icons/md";
 
-const Button = () => {
+const Button = (props) => {
+  console.log(props)
   return (
     <>
       <button className={`${styles.primary_btn}`}>
-        <MdMessage fontSize="24px" />
-        Via Support Chat
+        
+        {props.icon}
+        {props.text}
       </button>
     </>
   );
